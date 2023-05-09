bea207857df940e5834c8a37e3a396b32394ac59
dzgierski19 <dzgierskigit@gmail.com>
2023-05-09 21:36:55 +0200

    deleted emptyArray from some and every

--- masterReduce.ts
+++ masterReduce.ts
@@ -1,51 +1,39 @@
 const data = [5, 23, 5.5, 688];
 
-const emptyArray: number[] = [];
+// const mapFn = <T, K>(
+//   array: T[],
+//   callback: (element: T, index: number) => T
+// ): (T | K)[] => {
+//   // const newEmptyArray: T[] = [];
+//   return array.reduce(
+//     (accumulator: (T | K)[], currentValue: T, currentIndex: number) => {
+//       accumulator = callback(currentValue, currentIndex);
+//       newEmptyArray.push(accumulator);
+//       return accumulator;
+//     },
+//     []
+//   ); //tu cos zmienic
+//   // return newEmptyArray;
+// };
 
-const arrayReduce = (array: any[]) => {
-  data.reduce(
-    (accumulator: number, currentValue: number, currentIndex: number) => {
-      accumulator = currentValue * 2;
-      emptyArray.push(accumulator);
-      return accumulator;
-    },
-    0
-  );
-  return emptyArray;
-};
+// console.log(mapFn(data, (element) => element / 2));
 
-console.log(arrayReduce(data));
+// const filterFn = <T>(
+//   array: T[],
+//   callback: (element: T, index: number) => boolean
+// ): boolean => {
+//   return array.reduce(
+//     (accumulator: T, currentValue: T, currentIndex: number) => {
+//       if (callback(currentValue, currentIndex)) {
+//         return true;
+//       }
+//       return false;
+//     },
+//     true
+//   );
+// };
 
-const mapFn = <T>(
-  array: T[],
-  callback: (element: T, index: number) => T
-): T[] => {
-  const newEmptyArray: T[] = [];
-  array.reduce((accumulator: T, currentValue: T, currentIndex: number) => {
-    accumulator = callback(currentValue, currentIndex);
-    newEmptyArray.push(accumulator);
-    return accumulator;
-  }, array[0]);
-  return newEmptyArray;
-};
-
-console.log(mapFn(data, (element) => element / 2));
-
-const filterFn = <T>(
-  array: T[],
-  callback: (element: T, index: number) => boolean
-): T[] => {
-  const newEmptyArray: T[] = [];
-  array.reduce((accumulator: T, currentValue: T, currentIndex: number) => {
-    if (callback(currentValue, currentIndex)) {
-      newEmptyArray.push(currentValue);
-    }
-    return accumulator;
-  }, array[0]);
-  return newEmptyArray;
-};
-
-console.log(filterFn(data, (element) => element < 24));
+// console.log(filterFn(data, (element) => element < 24));
 
 console.log("every");
 
@@ -53,41 +41,36 @@ const everyFn = <T>(
   array: T[],
   callback: (element: T, index: number) => boolean
 ): boolean => {
-  const newEmptyArray: T[] = [];
-  array.reduce((accumulator: T[], currentValue: T, currentIndex: number) => {
-    if (callback(currentValue, currentIndex)) {
-      newEmptyArray.push(currentValue);
-    }
-    return accumulator;
-  }, []);
-  console.log(newEmptyArray);
-  console.log(newEmptyArray.length);
-  if (newEmptyArray.length === array.length) {
-    return true;
-  }
-  return false;
+  return array.reduce(
+    (accumulator: boolean, currentValue: T, currentIndex: number) => {
+      if (callback(currentValue, currentIndex)) {
+        return accumulator;
+      } else {
+        return false;
+      }
+    },
+    true
+  );
 };
 
-console.log(everyFn(data, (element) => element >= 5));
+console.log(everyFn(data, (element) => element < 65));
 
 console.log("some");
+
 const someFn = <T>(
   array: T[],
   callback: (element: T, index: number) => boolean
 ): boolean => {
-  const newEmptyArray: T[] = [];
-  array.reduce((accumulator: T, currentValue: T, currentIndex: number) => {
-    if (callback(currentValue, currentIndex)) {
-      newEmptyArray.push(currentValue);
-    }
-    return accumulator;
-  }, array[0]);
-  console.log(newEmptyArray);
-  console.log(newEmptyArray.length);
-  if (newEmptyArray.length >= 1) {
-    return true;
-  }
-  return false;
+  return array.reduce(
+    (accumulator: boolean, currentValue: T, currentIndex: number) => {
+      if (!callback(currentValue, currentIndex)) {
+        return accumulator;
+      } else {
+        return true;
+      }
+    },
+    false
+  );
 };
 
-console.log(someFn(data, (element) => element > 5.5));
+console.log(someFn(data, (element) => element > 650));
